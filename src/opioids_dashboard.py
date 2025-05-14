5d634edc9e04dee6a0adfad49f0e50c93edb17da
cubicle24 <brodyharttman@gmail.com>
2025-05-14 01:10:51 -0700

    finished the data analysis and making charts in jupyternotebook, building the dashboard now

--- src/opioids_dashboard.py
+++ src/opioids_dashboard.py
@@ -27,40 +27,10 @@ df = df.sample(n=20000,random_state=64)
 st.write(f"In-memory size: {df.memory_usage(deep=True).sum() / 1024**2:.2f} MB")
 
 
-st.markdown('<h1 style="text-align: center; margin-bottom: 0.5rem;">Merit-Based Incentive Payment System (MIPS) Dashboard</h1>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: center; font-size:15px; color:blue;">Explore trends and patterns in MIPS scores </div>', unsafe_allow_html=True)
-st.markdown('<div style="text-align: center; font-size:15px; color:lightblue;">Note: data is subsampled due to Streamlit cloud memory constraints. Full dataset is 25x larger.</div>', unsafe_allow_html=True)
+st.markdown('<h1 style="text-align: center; margin-bottom: 0.5rem;">US Opioid Prescribing Patterns by Provider Specialty Dashboard</h1>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center; font-size:15px; color:blue;">Explore geospatial patterns in how narcotics are prescribed</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center; font-size:15px; color:lightblue;">Note: data is sampled due to Streamlit cloud memory constraints. Full dataset is 25x larger.</div>', unsafe_allow_html=True)
 st.divider()
-
-
-#-- MAIN CONTENT ---
-leftbar, vertical_divider, col1, col2 = st.columns([1,0.05,1,3],gap="large")
-
-leftbar.subheader("Filter Providers by:")
-
-states = ['All'] + sorted(df['st'].dropna().unique().tolist())
-selected_state = leftbar.selectbox("State", options=states, index=0)
-print(f"Selected state: {selected_state}")
-
-specialties = ['All'] + sorted(df['pri_spec'].dropna().unique().tolist())
-selected_specialty = leftbar.selectbox("Specialty", options=specialties, index=0)
-print(f"Selected specialty: {selected_specialty}")
-
-genders = ['All'] + sorted(df['gndr'].dropna().str.strip().unique().tolist())
-selected_gender = leftbar.selectbox("Gender", options=genders, index=0)
-print(f"Selected gender: {selected_gender}")
-
-years_exp = ['All'] + sorted(df['years_experience'].dropna().unique().tolist())
-selected_years_exp = leftbar.selectbox("Years Experience", options=years_exp, index=0)
-print(f"Selected gender: {selected_years_exp}")
-
-med_school = ['All'] + sorted(df['Med_sch'].dropna().unique().tolist())
-selected_school = leftbar.selectbox("Medical School", options=med_school, index=0)
-print(f"Selected medical school: {selected_school}")
-
-size = ['All'] + sorted(df['num_org_mem'].dropna().unique().tolist())
-selected_size = leftbar.selectbox("Practice Size", options=size, index=0)
-print(f"Selected size: {selected_size}")
 
 @st.cache_data
 def filter_data(df, state, specialty, gender, years_exp, med_school,practice_size):
@@ -68,105 +38,109 @@ def filter_data(df, state, specialty, gender, years_exp, med_school,practice_siz
     if state != 'All':
         filtered = filtered[filtered['st'] == state]
     if specialty != 'All':
-        filtered = filtered[filtered['pri_spec'] == specialty]
-    if gender != 'All':
-        filtered = filtered[filtered['gndr'].str.strip() == gender]
+        filtered = filtered[filtered['Prscrbr_Type'] == specialty]
+    # if gender != 'All':
+    #     filtered = filtered[filtered['gndr'].str.strip() == gender]
     if years_exp != 'All':
         filtered = filtered[filtered['years_experience'] == years_exp]
-    if med_school != 'All':
-        filtered = filtered[filtered['Med_sch'] == med_school]
-    if practice_size != 'All':
-        filtered = filtered[filtered['num_org_mem'] == practice_size]
+    # if med_school != 'All':
+    #     filtered = filtered[filtered['Med_sch'] == med_school]
+    # if practice_size != 'All':
+    #     filtered = filtered[filtered['num_org_mem'] == practice_size]
     return filtered
 
-filtered_df = filter_data(df, selected_state, selected_specialty, selected_gender, selected_years_exp, selected_school, selected_size)
+#--SIDEBAR--
+with st.sidebar:
+    st.title("Filter by Specialty")
 
-#add in memory usage
-process = psutil.Process(os.getpid())
-memory_mb = process.memory_info().rss / 1024 / 1024
+# states = ['All'] + sorted(df['Prscrbr_State_Abrvtn'].dropna().unique().tolist())
+# selected_state = st.sidebar.selectbox("State", options=states, index=0)
+# print(f"Selected state: {selected_state}")
 
-st.metric(label="This Dashboard's Memory Usage", value=f"{memory_mb:.2f} MB")
+    specialties = ['All'] + sorted(df['Prscrbr_Type'].dropna().unique().tolist())
+    selected_specialty = st.selectbox("Specialty", options=specialties, index=0)
+    print(f"Selected specialty: {selected_specialty}")
+
+# genders = ['All'] + sorted(df['gndr'].dropna().str.strip().unique().tolist())
+# selected_gender = sidebar.selectbox("Gender", options=genders, index=0)
+# print(f"Selected gender: {selected_gender}")
+
+# years_exp = ['All'] + sorted(df['years_experience'].dropna().unique().tolist())
+# selected_years_exp = st.sidebar.selectbox("Years Experience", options=years_exp, index=0)
+# print(f"Selected gender: {selected_years_exp}")
+
+# med_school = ['All'] + sorted(df['Med_sch'].dropna().unique().tolist())
+# selected_school = sidebar.selectbox("Medical School", options=med_school, index=0)
+# print(f"Selected medical school: {selected_school}")
+
+# size = ['All'] + sorted(df['num_org_mem'].dropna().unique().tolist())
+# selected_size = sidebar.selectbox("Practice Size", options=size, index=0)
+# print(f"Selected size: {selected_size}")
 
 
-#-- KPI like Metrics ---
-def metric_card(label, value, color="#23272b", text_color="#fff", icon=None):
-    html = f"""
-    <div style="
-        background-color: {color};
-        padding: 20px 20px 12px 20px;
-        border-radius: 12px;
-        box-shadow: 0 2px 8px rgba(0,0,0,0.08);
-        margin-bottom: 7px;
-        margin-top: 10px;
-        min-width: 180px;
-        text-align: center;
-    ">
-        <div style="font-size: 1.1rem; color: #b0b0b0; margin-bottom: 6px;">
-            {icon if icon else ""} {label}
-        </div>
-        <div style="font-size: 2.2rem; font-weight: bold; color: {text_color};">
-            {value}
-        </div>
-    </div>
-    """
-    st.markdown(html, unsafe_allow_html=True)
 
-with vertical_divider:
-    st.markdown(
-        """
-        <div style="height: 100vh; border-left: 2px solid #e0e0e0; margin-left: auto; margin-right: auto;"></div>
-        """,
-        unsafe_allow_html=True
-    )
+    # filtered_df = filter_data(df, selected_state, selected_specialty, selected_gender, selected_years_exp, selected_school, selected_size)
+    filtered_df = filter_data(df, None, selected_specialty, None, None, None, None)
+
+    st.divider()
+
+    #add in memory usage
+    process = psutil.Process(os.getpid())
+    memory_mb = process.memory_info().rss / 1024 / 1024
+
+    st.metric(label="This Dashboard's Memory Usage", value=f"{memory_mb:.2f} MB")
+
+#-- MAIN CONTENT ---
+col1, col2 = st.columns([1,3],gap="large")
+
 
 with col1:
-    metric_card("Number of Providers", filtered_df['NPI'].nunique(), color="#f2f6f7", text_color="#23272b")
-    metric_card("Average MIPS Score", f"{filtered_df['final_MIPS_score'].mean():.1f}", color="#f2f6f7", text_color="#23272b")
-    metric_card("Number of Specialties", filtered_df['pri_spec'].nunique(), color="#f2f6f7", text_color="#23272b")
-    metric_card("Males: Females", calculate_gender_distribution(filtered_df), color="#f2f6f7", text_color="#23272b")
-    metric_card("Average Years Experience", f"{filtered_df['years_experience'].mean():0.1f}", color="#f2f6f7", text_color="#23272b")
-    metric_card("Average Practice Size", f"{filtered_df['num_org_mem'].mean():0.1f}", color="#f2f6f7", text_color="#23272b")
+    dbu.metric_card("Number of Providers", filtered_df['PRSCRBR_NPI'].nunique(), color="#f2f6f7", text_color="#23272b")
+    # dbu.metric_card("Number of Specialties", filtered_df['pri_spec'].nunique(), color="#f2f6f7", text_color="#23272b")
+    # dbu.metric_card("Males: Females", calculate_gender_distribution(filtered_df), color="#f2f6f7", text_color="#23272b")
+    # dbu.metric_card("Average Years Experience", f"{filtered_df['years_experience'].mean():0.1f}", color="#f2f6f7", text_color="#23272b")
+    # dbu.metric_card("Average Practice Size", f"{filtered_df['num_org_mem'].mean():0.1f}", color="#f2f6f7", text_color="#23272b")
 
-def make_mips_histogram(df, x_col, title, bar_color):
-    '''This function creates a histogram of the MIPS scores'''
-    fig = px.histogram(
-        df,
-        x=x_col,
-        nbins=110,
-        title=title,
-        marginal='box',  # Optional: adds a boxplot above
-        height=300,
-        width=500,
-        color_discrete_sequence=[bar_color]  # Use any hex color or named color
-    )
-    return fig
+# def make_mips_histogram(df, x_col, title, bar_color):
+#     '''This function creates a histogram of the MIPS scores'''
+#     fig = px.histogram(
+#         df,
+#         x=x_col,
+#         nbins=110,
+#         title=title,
+#         marginal='box',  # Optional: adds a boxplot above
+#         height=300,
+#         width=500,
+#         color_discrete_sequence=[bar_color]  # Use any hex color or named color
+#     )
+#     return fig
 
-with col2:
-    plot_df = filtered_df.sample(n=2000) if len(filtered_df) > 2000 else filtered_df
-    fig1 = make_mips_histogram(plot_df, 'final_MIPS_score', "Distribution of Overall Scores",'#25b5b9')
-    st.plotly_chart(fig1, use_container_width=True)
-    st.markdown("- Key Insight: MIPS by design clusters most providers around similar scores (mean 80), so most providers appear the same)")
+# with col2:
+#     plot_df = filtered_df.sample(n=2000) if len(filtered_df) > 2000 else filtered_df
+#     fig1 = make_mips_histogram(plot_df, 'final_MIPS_score', "Distribution of Overall Scores",'#25b5b9')
+#     st.plotly_chart(fig1, use_container_width=True)
+#     st.markdown("- Key Insight: MIPS by design clusters most providers around similar scores (mean 80), so most providers appear the same)")
 
-    #these show the breakdown in MIPS scores by Quality, PI, IA, and Cost
-    subcol1, subcol2 = st.columns(2)
+#     #these show the breakdown in MIPS scores by Quality, PI, IA, and Cost
+#     subcol1, subcol2 = st.columns(2)
 
-    with subcol1:
-        plot_df = filtered_df.sample(n=2000) if len(filtered_df) > 2000 else filtered_df
+#     with subcol1:
+#         plot_df = filtered_df.sample(n=2000) if len(filtered_df) > 2000 else filtered_df
        
-        fig2 = make_mips_histogram(plot_df, 'Quality_category_score', "Distribution of Quality Scores",'#b9c3eb')
-        st.plotly_chart(fig2, use_container_width=True)
+#         fig2 = make_mips_histogram(plot_df, 'Quality_category_score', "Distribution of Quality Scores",'#b9c3eb')
+#         st.plotly_chart(fig2, use_container_width=True)
 
-        fig3 = make_mips_histogram(plot_df, 'IA_category_score', "Distribution of Improvement Activities Scores",'#f83a3e')
-        st.plotly_chart(fig3, use_container_width=True)
-        st.markdown("- Key Insight: Improvement Activity scores are all similar, but low (40)")
+#         fig3 = make_mips_histogram(plot_df, 'IA_category_score', "Distribution of Improvement Activities Scores",'#f83a3e')
+#         st.plotly_chart(fig3, use_container_width=True)
+#         st.markdown("- Key Insight: Improvement Activity scores are all similar, but low (40)")
 
-    with subcol2:
-        plot_df = filtered_df.sample(n=2000) if len(filtered_df) > 2000 else filtered_df
+#     with subcol2:
+#         plot_df = filtered_df.sample(n=2000) if len(filtered_df) > 2000 else filtered_df
         
-        fig4 = make_mips_histogram(plot_df, 'PI_category_score', "Distribution of Promoting Interoperability Scores",'#39c3eb')
-        st.plotly_chart(fig4, use_container_width=True)
-        st.markdown("- Key Insight: Almost everyone scored very high (receiving credit often involves just checking boxes)")
+#         fig4 = make_mips_histogram(plot_df, 'PI_category_score', "Distribution of Promoting Interoperability Scores",'#39c3eb')
+#         st.plotly_chart(fig4, use_container_width=True)
+#         st.markdown("- Key Insight: Almost everyone scored very high (receiving credit often involves just checking boxes)")
 
-        fig5 = make_mips_histogram(plot_df, 'Cost_category_score', "Distribution of Cost Scores",'#fde23a')
-        st.plotly_chart(fig5, use_container_width=True)
+#         fig5 = make_mips_histogram(plot_df, 'Cost_category_score', "Distribution of Cost Scores",'#fde23a')
+#         st.plotly_chart(fig5, use_container_width=True)
 
